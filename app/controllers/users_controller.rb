63f4680f2f775e44e8e28f5ae1f51f56b399605e
pooja <poojaanbuchezhian@gmail.com>
2020-06-05 10:59:18 +0530

    Blog Assignment-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:firstname, :lastname)
+      params.require(:user).permit(:first_name, :last_name)
     end
 end
